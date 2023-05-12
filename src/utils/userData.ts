35c7fa10eae4724e2ea1c9bdd3fb84065d43c429
juliomello93 <juliomello@live.com>
2023-05-12 10:23:28 -0300

    feat:update userdata

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
-  facebookUser: "",
+  nameUser: "Julio Mello",
+  githubUser: "juliomello93",
+  linkedinUser: "juliomello93",
+  whatsappNumber: "+5517982060123",
+  emailUser: "juliomello@live.com",
+  instagramUser: "julinhomello",
+  facebookUser: "julio.mello.10",
 };
